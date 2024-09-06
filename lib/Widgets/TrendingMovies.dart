816517a281e0a3e6a42d51eb679cdade3915b02d
Mahmoud <mahmoud@localhost.localdomain>
2024-09-06 23:34:10 +0300

    on tap added

--- lib/Widgets/TrendingMovies.dart
+++ lib/Widgets/TrendingMovies.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:moviemania/Widgets/Constants.dart';
 import 'package:moviemania/Models/Movie.dart';
+import 'package:moviemania/Widgets/DetailsScreen.dart';
 
 class TrendingMovies extends StatelessWidget {
   TrendingMovies({
@@ -29,29 +30,37 @@ class TrendingMovies extends StatelessWidget {
     return SizedBox(
       width: double.infinity,
       child: CarouselSlider.builder(
-        itemCount: snapshot.data!.length ,
+        itemCount: snapshot.data!.length,
         itemBuilder: (context, itemIndex, pageViewIndex) {
-          return ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: SizedBox(
-                height: 300,
-                width: 200,
-                child: Image.network(
-                  filterQuality: FilterQuality.high,
-                  fit:BoxFit.cover,
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                           DetailsScreen(movie: snapshot.data[itemIndex])));
+            },
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: SizedBox(
+                  height: 300,
+                  width: 200,
+                  child: Image.network(
+                    filterQuality: FilterQuality.high,
+                    fit: BoxFit.cover,
                     '${Constants.imagePath}${snapshot.data[itemIndex].posterPath}',
+                  ),
+                )
+                /*Image.asset(
+                images[itemIndex],
+                fit: BoxFit.cover,
+                height: 100,
+
+              ),
+              */
 
                 ),
-              )
-              /*Image.asset(
-              images[itemIndex],
-              fit: BoxFit.cover,
-              height: 100,
-
-            ),
-            */
-
-              );
+          );
         },
         options: CarouselOptions(
           height: 300,
